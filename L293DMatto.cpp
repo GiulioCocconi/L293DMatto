109319fa7812783b5e7d605451d5e576a2db6915
GiulioCocconi <31406038+GiulioCocconi@users.noreply.github.com>
2021-03-02 21:13:29 +0100

    Update L293DMatto.cpp

--- L293DMatto.cpp
+++ L293DMatto.cpp
@@ -1,1 +1,31 @@
 #include "L293DMatto.h"
+
+L293DMatto::L293DMatto(int enable, int dirA, int dirB) {
+  this->enable = enable;
+  this->dirA = dirA;
+  this->dirB = dirB;
+  pinMode(enable, OUTPUT);
+  pinMode(positivo, OUTPUT);
+  pinMode(negativo, OUTPUT);
+  setDir(true);
+  setVel(127); //Metà della velocità massima
+}
+
+void L293DMatto::setDir(bool dir) {
+  this->dir = dir;
+  digitalWrite(dirA, dir);
+  digitalWrite(dirB, !dir);
+}
+
+bool L293DMatto::getDir() {
+  return this->dir;
+}
+
+void L293DMatto::setVel(int vel) {
+  this->vel = vel
+  analogWrite(this->enable, vel);
+}
+
+int L293DMatto::getVel() {
+  return this->vel;
+}
