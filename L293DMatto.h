2d619e3336a185f89ff5f0bcf54dd3d9ef6ed465
GiulioCocconi <31406038+GiulioCocconi@users.noreply.github.com>
2021-03-02 20:56:50 +0100

    Update L293DMatto.h

--- L293DMatto.h
+++ L293DMatto.h
@@ -3,5 +3,19 @@
 
 #include "Arduino.h"
 
+class L293DMatto {
+  public:
+    L293DMatto(int enable, int positivo, int negativo);
+    void setVel(int vel);
+    void setDir(bool dir);
+    int getVel();
+    bool getDir();
+  private:
+    int enable;
+    int positivo;
+    int negativo;
+    int vel;
+    int dir
+};
 
 #endif
