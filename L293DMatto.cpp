d7de075cb895219ad9b41122b0a5862ac9a7ccf7
Giulio Cocconi <31406038+GiulioCocconi@users.noreply.github.com>
2021-03-10 20:52:57 +0100

    Update L293DMatto.cpp

--- L293DMatto.cpp
+++ L293DMatto.cpp
@@ -4,6 +4,19 @@ L293DMatto::L293DMatto(int enable, int dirA, int dirB) {
   this->enable = enable;
   this->dirA = dirA;
   this->dirB = dirB;
+  this->min = 0;
+  pinMode(enable, OUTPUT);
+  pinMode(dirA, OUTPUT);
+  pinMode(dirB, OUTPUT);
+  setDir(true);
+  setVel(0);
+}
+
+L293DMatto::L293DMatto(int enable, int dirA, int dirB, int min) {
+  this->enable = enable;
+  this->dirA = dirA;
+  this->dirB = dirB;
+  this->min = min;
   pinMode(enable, OUTPUT);
   pinMode(dirA, OUTPUT);
   pinMode(dirB, OUTPUT);
@@ -25,16 +38,21 @@ bool L293DMatto::getDir() {
   return this->dir;
 }
 
-void L293DMatto::setVel(int vel) {
+void L293DMatto::setVelRaw(int vel) {
   if (vel<=255 && vel>=0) {
     this->vel = vel;
     analogWrite(this->enable, vel);
   }
 }
 
+void L293DMatto::setVel(int vel) {
+  vel = map(vel, 0, 255, this->min, 255);
+  this->setVelRaw(vel);
+}
+
 void L293DMatto::setVelPerc(int vel) {
-  int newVel = map(vel, 0, 100, 0, 255);
-  this->setVel(newVel);
+  vel = map(vel, 0, 100, this->min, 255);
+  this->setVelRaw(vel);
 }
 
 int L293DMatto::getVel() {
