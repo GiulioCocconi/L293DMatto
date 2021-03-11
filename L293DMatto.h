efd45cbf06f433f98174b773ef175cd6a987c9fb
Giulio Cocconi <31406038+GiulioCocconi@users.noreply.github.com>
2021-03-11 12:54:23 +0100

    Update L293DMatto.h

--- L293DMatto.h
+++ L293DMatto.h
@@ -6,12 +6,12 @@
 class L293DMatto {
   public:
     L293DMatto(int enable, int dirA, int dirB);
-    L293DMatto(int enable, int pin);
-    void setMin(int min);
-    void setVel(int vel);
+    L293DMatto(int enable, int pin); //Motore unidirezionale, consente di controllare fino a 4 motori con un solo driver. dirB sarà settato a 0
+    void setMin(int min); //Setta velocità minima
+    void setVel(int vel); //Setta la velocità tenendo conto del minimo in una funzione map
     void setDir(bool dir);
-    void setVelPerc(int vel);
-    void setVelRaw(int vel);
+    void setVelPerc(int vel); //Setta la velocità in percentuale
+    void setVelRaw(int vel); //Setta la velocità senza tenere conto del minimo
     int getVel();
     bool getDir();
   private:
